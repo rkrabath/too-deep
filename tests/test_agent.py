fa1c3c811dbbaa46441303b0d340b0a3b06ff7af
rkrabath <ryan@krabath.net>
2016-05-22 15:18:33 -0500

    Fix existing test and add a new one

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,12 +1,13 @@
 import entities as e
 import unittest
 import os
+from time import sleep
 
 class TestAgent(unittest.TestCase):
 	def setUp(self):
 		map_object = e.Map(10)
 		dispatch_object = e.Dispatch(map_object)
-		self.agent = e.Agent(e.Point(0,0,0), dispatch_object)
+		self.agent = e.Agent(e.Point(10,1,1), dispatch_object)
 	
 	
 	def tearDown(self):
@@ -16,10 +17,19 @@ class TestAgent(unittest.TestCase):
 	def test_subprocess(self):
 		assert os.getpid() != self.agent.process.pid
 
-	def test_move_during_tick(self)
+	def test_move_during_tick(self):
 		original_location = self.agent.location
+		self.agent.tick()
+		sleep(1)
 		self.agent.tick()
 		current_location = self.agent.location
 		assert current_location != original_location
+
+	def test_exit_of_child(self):
+		for x in xrange(0,40):
+			self.agent.tick()
+			sleep(.1)
+		assert not self.agent.alive
+		
 
 	
