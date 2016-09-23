72a64552e94203b32b608d7b73f993a2b8cb150a
rkrabath <ryan@krabath.net>
2016-09-22 22:22:58 -0500

    integrate into module

--- entities/__init__.py
+++ entities/__init__.py
@@ -3,6 +3,8 @@ from display import Display
 from point import Point
 from input import Input
 from agent import Agent
+from box import Box
+from compoundbox import CompoundBox
 
 # Not a class
 import map
