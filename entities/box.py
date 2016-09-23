c02ed14653714517b37097fe0473440c3d5658d5
rkrabath <ryan@krabath.net>
2016-09-23 00:06:23 -0500

    Some properties of boxes
    and tests for them that aren't passing

--- entities/box.py
+++ entities/box.py
@@ -7,6 +7,37 @@ class Box(object):
             raise ValueError("First parameter must be a Point")
         if type(bottom_right) != type(Point(0,0,0)):
             raise ValueError("Second parameter must be a Point")
+
         self.top_left = top_left
         self.bottom_right = bottom_right
 
+        self.left_edge = self.top_left.x
+        self.right_edge = self.bottom_right.x
+        self.top_edge = self.top_left.y
+        self.bottom_edge = self.bottom_right.y
+
+
+    def adjecent_to(self, other):
+        if self.left_edge == other.right_edge:
+            return True
+        if self.right_edge == other.left_edge:
+            return True
+        if self.top_edge == other.bottom_edge:
+            return True
+        if self.bottom_edge == other.top_edge:
+            return True
+
+        return False
+
+
+    def is_overlapping(self, other):
+        if self.left_edge < other.left_edge < self.right_edge:
+            return True
+        if self.right_edge > other.right_edge > self.left_edge:
+            return True
+        if self.top_edge < other.top_edge < self.bottom_edge:
+            return True
+        if self.bottom_edge > other.bottom_edge > self.top_edge:
+            return True
+
+        return False
--- tests/test_box.py
+++ tests/test_box.py
@@ -3,9 +3,38 @@ import unittest
 import os
 from time import sleep
 
+#
+# 1---+          
+# | A |     7-+
+# |   |     |D|  9---+
+# |   |     | |  |E  |
+# |   3-----+-8  |   |
+# |   | B   |    +---0
+# +---2   5-|--+
+#     |   | |C |
+#     |   +-|--6
+#     +-----4
+
+# 1: (1,1)
+# 2: (5,7)
+# 3: (5,5)
+# 4: (11,10)
+# 5: (9,7)
+# 6: (14,9)
+# 7: (11,2)
+# 8: (13,5)
+# 9: (16,3)
+# 0: (20,6)
+
 class TestBox(unittest.TestCase):
-#    def setUp(self):
+    def setUp(self):
+        self.A = e.Box(e.Point(0,1,1),e.Point(0,5,7))
+        self.B = e.Box(e.Point(0,5,5),e.Point(0,11,10))
+        self.C = e.Box(e.Point(0,9,7),e.Point(0,14,9))
+        self.D = e.Box(e.Point(0,11,2),e.Point(0,13,4))
+        self.E = e.Box(e.Point(0,16,3),e.Point(0,20,6))
 	
+
     def test_box_error_handling(self):
         self.assertRaises(TypeError, e.Box, (4))
         self.assertRaises(TypeError, e.Box, ([4,4]))
@@ -13,18 +42,18 @@ class TestBox(unittest.TestCase):
         self.assertRaises(ValueError, e.Box, (4,4,4), e.Point(0,4,4))
 
 
-    def test_compound_box_creation(self):
-        box1 = e.Box(e.Point(0,4,4),e.Point(0,5,5))
-        box2 = e.Box(e.Point(0,4,4),e.Point(0,4,5))
-        box3 = e.Box(e.Point(0,4,5),e.Point(0,6,6))
+    def test_adjacency(self):
+        assert self.A.adjecent_to(self.B)
+        assert not self.D.adjecent_to(self.E)
+        assert not self.B.adjecent_to(self.C)
 
-        result_1 = type(e.CompoundBox(box1).original_boxes)
-        result_2 = type(e.CompoundBox([box1, box2, box3]).original_boxes)
+    def test_overlapping(self):
+        assert self.B.is_overlapping(self.C)
+        assert not self.A.is_overlapping(self.B)
+        assert not self.B.is_overlapping(self.D)
+        assert not self.B.is_overlapping(self.E)
+        assert not self.D.is_overlapping(self.C)
+        assert not self.D.is_overlapping(self.E)
 
-#        assert result_1 == self.intended_type
-#        assert result_2 == self.intended_type
-		
-#    def test_compound_box_computation(self):
-#        boxes = 
 
 	
